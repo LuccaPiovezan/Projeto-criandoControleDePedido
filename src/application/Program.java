7035d8c510486fdd022264e89387d135bb7dc852
LuccaPiovezan <luccapiovezan@gmail.com>
2020-09-02 13:52:26 -0300

    Criando um controle de pedido

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,22 @@
 package application;
 
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+		
+		System.out.println("Order Status: " + order);
 
 	}
 
